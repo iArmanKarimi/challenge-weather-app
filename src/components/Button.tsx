7af67af34084fd1f0081e61bc66b3e756c2bbc42
Arman karimi <arman.ui.dev@gmail.com>
2026-02-22 00:54:01 +0330

    fix: tailwind class detection
    solution: use variant mapping in order to grant static predefined classes inside an object

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,27 @@
 type Props = {
 	text: string;
-	bg?: (string & {}) | "blue";
+	bg?: "blue";
+};
+
+const variantMap = {
+	blue: {
+		bg: "bg-blue-500",
+		hover: "hover:bg-blue-600",
+		focus: "focus:ring-blue-300",
+	},
 };
 
 export default function Button({ text, bg: variant = "blue" }: Props) {
 	return (
 		<button
-			className={`bg-${variant}-500 hover:bg-${variant}-800 text-white 
-				w-full sm:w-25 outline-offset-3 focus:outline-2 focus:outline-${variant}-500
-				font-bold py-2.5 px-4 rounded-lg`}
+			className={`
+				${variantMap[variant].bg}
+				${variantMap[variant].hover}
+				${variantMap[variant].focus}
+				text-white w-full sm:w-25
+				outline-offset-3 focus:outline-2
+				font-bold py-2.5 px-4 rounded-lg
+			`}
 		>
 			{text}
 		</button>

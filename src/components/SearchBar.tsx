61ed87a5e3367a8c99ea69ee17dd025d0476b903
Arman karimi <arman.ui.dev@gmail.com>
2026-02-21 18:03:01 +0330

    Create SearchBar.tsx

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,17 @@
+export default function SearchBar() {
+	return (
+		<div
+			className="flex items-center py-1 px-4 w-full sm:w-[50%]
+		bg-surface rounded-lg focus-within:ring-2 focus-within:ring-white
+		"
+		>
+			<img src="images/icon-search.svg" alt="" />
+			<input
+				type="text"
+				placeholder="Search for a place..."
+				className="w-full sm:w-64 px-4 py-2 rounded-lg text-white
+				focus:outline-none "
+			/>
+		</div>
+	);
+}

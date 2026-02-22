14e3756bb1de0d6de53c6a785206ce79cf584595
Arman karimi <arman.ui.dev@gmail.com>
2026-02-23 01:30:19 +0330

    Refactor CityWeatherHeader layout and styles
    Adjust header layout and spacing for better centering and visual balance. Added justify-center and text-center, removed object-contain, and tweaked min height. Updated date color class, increased icon width (w-18 → w-20) and degree size (text-5xl → text-7xl), and added spacing (mt-4, mr-5) to improve alignment.

--- src/components/CityWeatherHeader.tsx
+++ src/components/CityWeatherHeader.tsx
@@ -29,17 +29,18 @@ const iconMap = {
 export default function CityWeatherHeader({ city, date, degree, icon }: Props) {
 	return (
 		<div
-			className={`flex flex-col items-center gap-1 w-full rounded-xl sm:rounded-2xl
-				object-contain mt-5 min-h-60
+			className={`flex flex-col items-center justify-center 
+				gap-1 w-full rounded-xl sm:rounded-2xl
+				mt-5 min-h-55 text-center
 				bg-[url('images/bg-today-small.svg')] 
 				sm:bg-[url('images/bg-today-large.svg')]
 				bg-cover bg-center`}
 		>
 			<h1 className="text-3xl font-bold">{city}</h1>
-			<p className="text-sm text-neutral-500">{date}</p>
-			<div className="flex flex-row items-center justify-center">
-				<img className="w-18" src={iconMap[icon]} alt={icon} />
-				<p className="text-5xl font-semibold italic">{degree}°</p>
+			<p className="text-sm text-text-secondary">{date}</p>
+			<div className="flex flex-row items-center justify-center mt-4">
+				<img className="w-20" src={iconMap[icon]} alt={icon} />
+				<p className="text-7xl font-semibold italic mr-5">{degree}°</p>
 			</div>
 		</div>
 	);

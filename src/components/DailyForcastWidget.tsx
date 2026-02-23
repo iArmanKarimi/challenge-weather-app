6ec53233809dffc7ea5700a894a755c5f69371fa
Arman karimi <arman.ui.dev@gmail.com>
2026-02-24 00:46:56 +0330

    add degree high/low

--- src/components/DailyForcastWidget.tsx
+++ src/components/DailyForcastWidget.tsx
@@ -4,18 +4,29 @@ import { type WeatherIcon, getWeatherIcon } from "../util/weatherIcon";
 type Props = {
 	day: string;
 	icon: WeatherIcon;
+	degreeHigh: number;
+	degreeLow: number;
 };
 
-export default function DailyForecastWidget({ day, icon }: Props) {
+export default function DailyForecastWidget({
+	day,
+	icon,
+	degreeHigh,
+	degreeLow,
+}: Props) {
 	return (
 		<WidgetSurface>
 			<div
 				className="text-center text-sm sm:text-lg
-			 	py-3 px-4"
+			 	mt-3 px-4"
 			>
 				{day}
 			</div>
 			<img src={getWeatherIcon(icon)} alt="icon" />
+			<div className="flex flex-row px-2 mb-2">
+				<span className="text-sm sm:text-lg">{degreeHigh}°</span>
+				<span className="text-sm sm:text-lg ml-auto">{degreeLow}°</span>
+			</div>
 		</WidgetSurface>
 	);
 }
--- src/components/DailyForecast.tsx
+++ src/components/DailyForecast.tsx
@@ -8,7 +8,13 @@ export default function DailyForecast() {
 			<h2 className="text-xl">Daily Forecast</h2>
 			<div className="mt-4 grid grid-cols-3 gap-2 sm:gap-4">
 				{weekdays.map((day) => (
-					<DailyForecastWidget key={day} day={day} icon="drizzle" />
+					<DailyForecastWidget
+						key={day}
+						day={day}
+						icon="drizzle"
+						degreeHigh={34}
+						degreeLow={12}
+					/>
 				))}
 			</div>
 		</div>

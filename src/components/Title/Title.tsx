6a2faee644da25ebae83ae54d6e4a0df2c75ee37
Arman karimi <arman.ui.dev@gmail.com>
2026-02-24 22:48:05 +0330

    style: use appropriate font

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,6 +1,6 @@
 export default function Title() {
 	return (
-		<h1 className="text-4xl font-bold mx-8 text-center my-4">
+		<h1 className="text-4xl font-bold mx-8 text-center my-4 font-display">
 			How's the sky looking today?
 		</h1>
 	);
